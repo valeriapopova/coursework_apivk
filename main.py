3e9163a311d067c17e10d5b7fe66adf9fd659192
valeriapopova <iamvaleriapopova@yandex.ru>
2021-09-18 16:47:56 +0400

    '8'

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
 import time
 from tqdm import tqdm
-import json
 from ya import YD
 from pprint import pprint
 
 version = '5.131'
 URL = 'https://api.vk.com/method/photos.get'
 token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-id = '552934290'
+id_ = '552934290'
+
 
 def getting_photos():
     params = {
-        'owner_id': id,
+        'owner_id': id_,
         'album_id': 'profile',
         'extended': '1',
         'access_token': token,
@@ -23,7 +23,6 @@ def getting_photos():
     return res.json()
 
 
-
 pprint(getting_photos())
 
 
@@ -32,7 +31,7 @@ def photos_list():
     photos_dict = {}
     for i in photos_json:
         likes = f"{i['likes']['count']}.jpg"
-        size = i['sizes'][-1]
+        size = i['sizes'][-1]['url']
         if likes in photos_dict:
             photos_dict[f"{i['date']}.jpg"] = size
         else:
@@ -44,11 +43,10 @@ def photos_list():
 pprint(photos_list())
 
 
-
 if __name__ == '__main__':
     vk = photos_list().keys()
     vk_path = photos_list().values()
     ya_token = ''
     uploader = YD(ya_token)
-    result = uploader.upload_file_to_disk(vk_path, vk)
+    result = uploader.upload_(vk_path, vk )
     pprint(result)
--- ya.py
+++ ya.py
@@ -13,19 +13,10 @@ class YD:
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
-    def get_upload_link(self, file_path):
+    def upload_(self, file_path, file_name):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        params = {"path": file_path, "overwrite": "true"}
-        response = requests.get(upload_url, headers=headers, params=params)
-        pprint(response.json())
-        return response.json()
-
-    def upload_file_to_disk(self, file_path, filename):
-        href = self.get_upload_link(file_path=file_path).get("href", "")
-        response = requests.put(href, filename)
-        response.raise_for_status()
-        if response.status_code == 201:
-            print("Success")
-
-
+        params = {"path": file_name, "url": file_path, "overwrite": "true"}
+        response = requests.post(upload_url, headers=headers, params=params)
+        res = response.json()
+        pprint(res)
