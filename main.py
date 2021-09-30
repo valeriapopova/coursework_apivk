f005f9d73b8f3c88fa4b780f314264963476a6fc
valeriapopova <iamvaleriapopova@yandex.ru>
2021-09-30 17:17:15 +0400

    'final'

--- main.py
+++ main.py
@@ -4,24 +4,44 @@ from ya import YD
 from pprint import pprint
 from tqdm import tqdm
 import time
+import json
 
 version = '5.131'
 URL = 'https://api.vk.com/method/photos.get'
+id_ = input('Введите username или id:')
+count_ = input('Введите кол-во фото при загрузке:')
 token = ''
-id_or_username = ''
+
+
+def get_id():
+    _get_id = {}
+    url_ = 'https://api.vk.com/method/users.get'
+    params = {
+        'user_ids': id_,
+        'access_token': token,
+        'v': version
+    }
+    response = requests.get(url_, params=params).json()
+    for items in response['response']:
+        user_id = items['id']
+        _get_id = {'id': user_id}
+    return _get_id
 
 
 def getting_photos():
-    params = {
-        'owner_ids': id_or_username,
-        'album_id': 'profile',
-        'extended': '1',
-        'access_token': token,
-         'v': version,
-        'feed': ''
-    }
-    res = requests.get(URL, params=params, timeout=5)
-    return res.json()
+    data = get_id()
+    for value in data.items():
+        params = {
+            'owner_id': value,
+            'album_id': 'profile',
+            'extended': '1',
+            'access_token': token,
+            'v': version,
+            'feed': '',
+            'count': count_
+            }
+        res = requests.get(URL, params=params, timeout=5)
+        return res.json()
 
 
 def photos_list():
@@ -35,7 +55,6 @@ def photos_list():
             photos_dict[f"{d}.jpg"] = size
         else:
             photos_dict[likes] = size
-
     return photos_dict
 
 
@@ -51,3 +70,5 @@ if __name__ == '__main__':
         for i in tqdm(result, desc='Photo', unit=' photo'):
             time.sleep(0.25)
         pprint(result)
+    p = json.dumps(photos_list(), indent=4, ensure_ascii=False)
+    print(p)
